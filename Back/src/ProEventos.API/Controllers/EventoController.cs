412ab900919dfa770b1a9758c0f727e7a13a62c6
arinellipatrick <arinellidev@gmail.com>
2023-03-11 07:48:14 -0300

    Web .NET API in progress with Angular 12

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,63 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class EventoController : ControllerBase
+  {
+    
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 12 e .NET 5",
-                Local = "Rio de Janeiro",
-                Lote = "1o Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 12 e suas novidades",
-                Local = "São Paulo",
-                Lote = "2o Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto2.png"
-            }
-        };
-        public EventoController()
-        {
+      _context = context;
 
-        }
-       
-        [HttpGet]
-        public IEnumerable<Evento> Get()
-        {
-            return _evento;
-        }
-        [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
-        {
-            return _evento.Where(evento => evento.EventoId == id);
-        }
-        [HttpPost]
-        public string Post()
-        {
-            return "Post Example";
-        }
-        [HttpPut("{id}")]
-        public string Put(int id)
-        {
-            return $"Put Example with id = {id}";
-        }
-        [HttpDelete("{id}")]
-        public string Delete(int id)
-        {
-            return $"Delete Example with id = {id}";
-        }
     }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+      return _context.Eventos;
+    }
+    [HttpGet("{id}")]
+    public IEnumerable<Evento> GetById(int id)
+    {
+      yield return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+    }
+    [HttpPost]
+    public string Post()
+    {
+      return "Post Example";
+    }
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+      return $"Put Example with id = {id}";
+    }
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+      return $"Delete Example with id = {id}";
+    }
+  }
 }
--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -6,14 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API
 {
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,12 +24,16 @@ namespace ProEventos.API
             Configuration = configuration;
         }
 
+            
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
